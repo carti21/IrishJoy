e8ec138242743587ef2926da39e82607cb64d0fe
root <ardit.mustafaraj@gmail.com>
2015-06-21 14:08:38 +0200

    Social Media Area ready

--- view-image.php
+++ view-image.php
@@ -13,8 +13,6 @@
 		<?php /*show_meta_tags();*/ ?>
 		<link rel="stylesheet" type="text/css" href="css/css_public.css" />
 		<link rel="icon" href="<?php echo MAIN_URL; ?>images/favicon.png" type="image/x-icon"> 
-		<link rel="author" href="<?php echo MAIN_URL; ?>" />
-		<link rel="canonical" href="<?php echo MAIN_URL; ?>view-image.php?p_id=<?php echo"$id"?> " />
 	</head>
 	<body>
 		<div id="head">
@@ -40,31 +38,22 @@
 					</div>
 				</div>
 			</div>
-				<div id="ad_box">
-					<a href="http://www.flivetech.com/" target="_blank"> 
-						<img style="width:300px; height:auto;" src="images/flivetech.png"  alt="Flivetech.com - Flying Technology" >
-					</a>
-				</div>
-				<div id="ad_bottom">
-					<div style="margin-top:15px;"><a href"http://www.flivetech.com/">Our Partners</a></div>
-				</div>
-				<div id="before_menu">
-					<!-- <div style="margin-left:40px; margin-top: 10px; overflow-x: hidden;">
-						<div class="fb-like-box" data-href="http://www.facebook.com/irishjoycom" data-colorscheme="light" data-show-faces="false" 
-						data-header="true" data-stream="false" data-show-border="true">
-						</div> -->
-					<!-- <div style="margin-left:7px;" class="fb-follow" data-href="http://www.facebook.com/irishjoycom" 
-						data-colorscheme="light" data-layout="button_count" data-show-faces="true"></div>
-					</div> --> 
-				</div>
-					<div id="menu_bar">
-						<?php show_main_menu() ?>
-					</div>
-				</div>  
- 		</div>
- 		</div>
- 
- 		<div id="footer" style="background-color:#383838;"> 
+			<div id="ad_box">
+				<a href="http://www.flivetech.com/" target="_blank"> 
+					<img style="width:300px; height:auto;" src="images/flivetech.png"  alt="Flivetech.com - Flying Technology" >
+				</a>
+			</div>
+			<div id="ad_bottom">
+				<div style="margin-top:15px;"><a href"http://www.flivetech.com/">Our Partners</a></div>
+			</div>
+			<div id="before_menu">
+				<!-- Social Media Buttons goes here -->
+			</div>
+			<div id="menu_bar">
+				<?php show_main_menu() ?>
+			</div>
+		</div>  
+  		<div id="footer" style="background-color:#383838;"> 
  			<div id="copyright">&copy; www.irishjoy.com 2014 &nbsp;&nbsp;&nbsp; <a target="_blank" href="<?php echo MAIN_URL; ?>"> www.facebook.com/irishjoycom</a></div>
  		</div>
 	</body>
