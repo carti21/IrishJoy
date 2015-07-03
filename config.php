1145ec5870f3123eebeaf692e2faf605a43e4543
root <ardit.mustafaraj@gmail.com>
2015-07-03 23:54:36 +0200

    little changes on config. organizing the constants and commented

--- config.php
+++ config.php
@@ -1,17 +1,25 @@
 <?php 
 
 	define('MAIN_URL', 'http://localhost/irishjoy/');
-	define('UPLOADS_URL', MAIN_URL.'/uploads/');
 
-    define('PANEL_URL', 'http://localhost/irishjoy/panel/');
-    define('GOOGLE_ANALYTICS_URL', '/irishjoy/');
-    define('PHPMYADMIN_URL', 'localhost/phpmyadmin/');
-    define('EMAIL_URL', '/irishjoy/');
-    define('SERVER_URL', dirname( __FILE__ ).'/');
-
-	define("HOST", "localhost"); // The host you want to connect to.
-	define("USER", "root"); // The database username.
-	define("PASSWORD", "112"); // The database password. 
-	define("DATABASE", "irishjoy"); // The database name.
-	  
+	/*
+	Database Configurations
+	*/
+	define('HOST', 'localhost'); // The host you want to connect to.
+	define('USER', 'root'); // The database username.
+	define('PASSWORD', '112'); // The database password. 
+	define('DATABASE', 'irishjoy'); // The database name.
 	$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+	
+	/*
+	Paths - Changing not Recommanded. Only if changing folder structure directory
+	*/
+	define('UPLOADS_URL', MAIN_URL.'/uploads/');
+	define('PANEL_URL', MAIN_URL.'panel/');
+	define('SERVER_URL', dirname( __FILE__ ).'/');
+	
+	define('GOOGLE_ANALYTICS_URL', '/irishjoy/');
+	define('PHPMYADMIN_URL', 'localhost/phpmyadmin/');
+	define('EMAIL_URL', '/irishjoy/');
+	
+	
