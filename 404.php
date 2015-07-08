f3041cefa8bb436842da0d2751029f0ec475893b
root <ardit.mustafaraj@gmail.com>
2015-07-08 20:34:45 +0200

    added .htaccess and 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<div style="width:400px; height:400px; margin:150px auto;" >
+			<h2 style="font-family: arial;"> Page Not Found! </h2>
+		</div>
+	</body>
+</body>
